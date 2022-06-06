a27a874a9d0eae62b2bf40a37c7f5e241ffa8eb1
joseph shahin <josephchahine@live.com>
2022-06-06 17:31:00 +0200

    added the basic needed variables to the quiz

--- quiz.js
+++ quiz.js
@@ -1,3 +1,10 @@
 const question = document.getElementById("question");
 const answer = Array.from(document.getElementsByClassName('answer-text'));
 
+let currentQuestion = {} ;
+let acceptingAnswers = true;
+let score = 0;
+let questionCount = 0;
+let availableQuestions = [];
+
+let questions = [];
