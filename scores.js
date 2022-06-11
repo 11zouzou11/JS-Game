76310b1458ed4479205861b7eeb2c22ea3d12ac2
joseph shahin <josephchahine@live.com>
2022-06-11 23:43:20 +0200

    added the styling to the scores page with the js script to save the high scores and display them on the scores page

---
+++ scores.js
@@ -0,0 +1,8 @@
+const scoresList = document.getElementById('scoresList');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+scoresList.innerHTML = highScores
+    .map(score => {
+        return `<li class="high-score">${score.name} : ${score.score}</li>`;
+    }).join("")
+    ;
